1c044f29144307ad6587794ae2ec2aa101f57828
bzareba <bzareba21@gmail.com>
2021-04-16 20:47:12 +0200

    Add pokeviz

---
+++ pokeviz/counters.js
@@ -0,0 +1,53 @@
+TYPE_COUNTERS = ["Bug",
+"Dark Grass Psychic", "Fighting Grass Ground", "",
+ 
+"Dark",
+"Ghost Psychic", "Dark Ghost", "Psychic",
+ 
+"Dragon",
+"Dragon", "Electric Fire Grass Water", "",
+ 
+"Electric", 
+ "Flying Water", "Electric Flying Steel", "",
+ 
+"Fairy", 
+ "Dark Dragon Fighting", "Bug Dark Fighting", "Dragon",
+ 
+"Fighting", 
+ "Dark Ice Normal Rock Steel", "Bug Dark Rock", "",
+ 
+"Fire",
+"Bug Grass Ice Steel", "Bug Fire Fairy Grass Ice Steel", "",
+ 
+"Flying",
+"Bug Fighting Grass", "Bug Fighting Grass", "Ground",
+ 
+"Ghost", 
+"Psychic", "Bug Poison", "Fighting Normal",
+ 
+"Grass", 
+"Ground Rock Water", "Electric Grass Ground Water", "",
+ 
+"Ground",
+"Electric Fire Poison Rock Steel", "Poison Rock", "Electric",
+ 
+"Ice",
+"Dragon Flying Grass Ground", "Ice", "",
+ 
+"Normal",
+ "", "", "Ghost",
+ 
+"Poison",
+"Grass Fairy", "Bug Fighting Fairy Grass Poison", "",
+ 
+"Psychic",
+"Fighting Poison", "Fighting Psychic", "",
+ 
+"Rock", 
+"Bug Fire Flying Ice", "Fire Flying Normal Poison", "",
+ 
+"Steel", 
+"Fairy Ice Rock", "Steel Fire Water Electric", "Poison",
+ 
+"Water",
+"Fire Ground Rock", "Fire Ice Steel Water", ""]
---
+++ pokeviz/counters2.js
@@ -0,0 +1,55 @@
+TYPE_COUNTERS = [
+"Bug",
+"Dark Grass Psychic", "Fighting Fire Flying Fairy Ghost Poison Steel", "",
+
+"Dark",
+"Ghost Psychic", "Dark Fighting Fairy", "",
+
+"Dragon",
+"Dragon", "Steel", "Fairy",
+
+"Electric",
+"Flying Water", "Dragon Electric Grass", "Ground",
+
+"Fairy",
+"Dark Dragon Fighting", "Fire Poison Steel", "",
+
+"Fighting",
+"Dark Ice Normal Rock Steel", "Bug Fairy Flying Poison Psychic", "Ghost",
+
+"Fire",
+"Bug Grass Ice Steel", "Dragon Fire Rock Water", "",
+
+"Flying",
+"Bug Fighting Grass", "Electric Rock Steel", "",
+
+"Ghost",
+"Ghost Psychic", "Dark", "Normal",
+
+"Grass",
+"Ground Rock Water", "Bug Dragon Fire Flying Grass Poison Steel", "",
+
+"Ground",
+"Electric Fire Poison Rock Steel", "Bug Grass", "Flying",
+
+"Ice",
+"Dragon Flying Grass Ground", "Fire Ice Steel Water", "",
+
+"Normal",
+"", "Rock Steel", "Ghost",
+
+"Poison",
+"Grass Fairy", "Ghost Ground Poison Rock", "Steel",
+
+"Psychic",
+"Fighting Poison", "Psychic Steel", "Dark",
+
+"Rock",
+"Bug Fire Flying Ice", "Fighting Ground Steel", "",
+
+"Steel",
+"Fairy Ice Rock", "Electric Fire Steel Water", "",
+
+"Water",
+"Fire Ground Rock", "Dragon Grass Water", ""
+]
